d909dd607d7f42f742eec3973f2fb93cd1216e76
Marian Busila <marian.busila@gmail.com>
2019-01-13 02:35:02 -0500

    create employee repository

---
+++ EmployeeReact/Models/EmployeeRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeReact.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly POCContext dbContext;
+
+        public EmployeeRepository(POCContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Delete(int id)
+        {
+            var employee = this.dbContext.Employee.First(it => it.EmployeeId == id);
+            this.dbContext.Employee.Remove(employee);
+            this.dbContext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+        {
+            return this.dbContext.Employee.FirstOrDefault(it => it.EmployeeId == id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return this.dbContext.Employee.AsEnumerable();
+        }
+
+        public int Insert(Employee employee)
+        {
+            this.dbContext.Employee.Add(employee);
+            this.dbContext.SaveChanges();
+            return employee.EmployeeId;
+        }
+
+        public void Update(Employee employee)
+        {
+            this.dbContext.Employee.Update(employee);
+            this.dbContext.SaveChanges();
+        }
+    }
+}
---
+++ EmployeeReact/Models/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeeReact.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employee Get(int id);
+        IEnumerable<Employee> GetAll();
+        int Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int id);
+    }
+}
