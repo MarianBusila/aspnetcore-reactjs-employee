8feb650e277ad16c109630e23ec6816e87801507
Marian Busila <marian.busila@gmail.com>
2019-01-13 00:39:57 -0500

    create models

---
+++ EmployeeReact/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeReact.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public int? CityId { get; set; }
+
+        public City City { get; set; }
+    }
+}
---
+++ EmployeeReact/Models/POCContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeReact.Models
+{
+    public partial class POCContext : DbContext
+    {
+        public POCContext()
+        {
+        }
+
+        public POCContext(DbContextOptions<POCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<City> City { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-SJVR70F;Database=POC;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LasttName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.City)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.CityId)
+                    .HasConstraintName("FK_Employee_City_Id");
+            });
+        }
+    }
+}
